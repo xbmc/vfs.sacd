e242912e7b7cabec1b72b3a81a44fbb5f41511bf
Daniel Scheller <d.scheller@gmx.net>
2017-05-01 18:32:19 +0200

    remove obsolete ADDON_* methods (fixes build after xbmc/xbmc#12011)

--- src/SACDFile.cpp
+++ src/SACDFile.cpp
@@ -227,15 +227,6 @@ void ADDON_Destroy()
   destroy_logging();
 }
 
-//-- HasSettings --------------------------------------------------------------
-// Returns true if this add-on use settings
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-bool ADDON_HasSettings()
-{
-  return false;
-}
-
 //-- GetStatus ---------------------------------------------------------------
 // Returns the current Status of this visualisation
 // !!! Add-on master function !!!
@@ -245,24 +236,6 @@ ADDON_STATUS ADDON_GetStatus()
   return ADDON_STATUS_OK;
 }
 
-//-- GetSettings --------------------------------------------------------------
-// Return the settings for XBMC to display
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-unsigned int ADDON_GetSettings(ADDON_StructSetting ***sSet)
-{
-  return 0;
-}
-
-//-- FreeSettings --------------------------------------------------------------
-// Free the settings struct passed from XBMC
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-
-void ADDON_FreeSettings()
-{
-}
-
 //-- SetSetting ---------------------------------------------------------------
 // Set a specific Setting value (called from XBMC)
 // !!! Add-on master function !!!
@@ -270,14 +243,6 @@ void ADDON_FreeSettings()
 ADDON_STATUS ADDON_SetSetting(const char *strSetting, const void* value)
 {
   return ADDON_STATUS_OK;
-}
-
-//-- Announce -----------------------------------------------------------------
-// Receive announcements from XBMC
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-void ADDON_Announce(const char *flag, const char *sender, const char *message, const void *data)
-{
 }
 
 struct SACDContext
