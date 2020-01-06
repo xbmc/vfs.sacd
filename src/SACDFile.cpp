ad3140ae98332aad2edc2d59e07b89770ce07677
Alwin Esch <alwin.esch@web.de>
2020-01-06 23:33:13 +0100

    update copyright year and names

--- src/SACDFile.cpp
+++ src/SACDFile.cpp
@@ -1,5 +1,7 @@
 /*
  *      Copyright (C) 2014 Arne Morten Kvarving
+ *      Copyright (C) 2014-2019 Team Kodi
+ *      http://kodi.tv
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -12,7 +14,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with XBMC; see the file COPYING.  If not, see
+ *  along with Kodi; see the file COPYING.  If not, see
  *  <http://www.gnu.org/licenses/>.
  *
  */
